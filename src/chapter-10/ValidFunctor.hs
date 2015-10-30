bc0ec7dabbc2c0122ecc980b744e339d74b0e326
EricYT <eric.yu@gmail.com>
2015-10-30 18:49:01 +0800

    chapter 10

---
+++ src/chapter-10/ValidFunctor.hs
@@ -0,0 +1,11 @@
+
+data Foo a = Foo a
+
+instance Functor Foo where
+    fmap f (Foo a) = Foo (f a)
+
+data (Eq a) => Bar a = Bar a
+-- data Bar a = Bar a
+
+instance Functor Bar where
+    fmap f (Bar a) = Bar (f a)
